9f8feb84bee82355481c66541cb559889192b6b5
Stepan Lavrentev <lawrentievsv@gmail.com>
2021-09-22 10:42:14 +0300

    add possible to set up wait_time
    Signed-off-by: Stepan Lavrentev <lawrentievsv@gmail.com>

--- src/locust_run.py
+++ src/locust_run.py
@@ -1,12 +1,14 @@
 import time
 import json
+import os
 from locust import HttpUser, task, between, events
 from locust.user.task import tag
+from locust.user.wait_time import constant
 from fixtures import stake_changes_by_address, history_changes_by_address
 
 
 class QuickstartUser(HttpUser):
-    wait_time = between(1, 5)
+    wait_time = constant(float(os.environ.get('WAIT_TIME', 1)))
     host = "https://api.subquery.network/sq/ef1rspb"
     headers = {"content-type": "application/json",
                "user-agent": "fearless/1 CFNetwork/978.0.7 Darwin/20.6.0",
